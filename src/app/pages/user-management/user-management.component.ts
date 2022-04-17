fd275aaba8c0a951540d8c68b0d496cfaea993c2
Albert Mayta Segovia <54085998+MSAlbert40@users.noreply.github.com>
2022-04-17 12:31:11 -0500

    fix: user-management - service connection
    Implementación de los servicios refactorizados para este proceso.

--- src/app/pages/user-management/user-management.component.ts
+++ src/app/pages/user-management/user-management.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { SemesterService } from 'src/app/core/services/semester.service';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-user-management',
@@ -7,80 +8,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-management.component.scss']
 })
 export class UserManagementComponent implements OnInit {
+
   page = 1;
-  number_projects: number = 50;
   pageSize = 15;
+  number_users: number;
   keyword: string = "";
+
   users: any[] = [];
-  semesters: any[] = [];
-  fake: any[] = [];
   filter: any[] = [];
-  isLoaded: boolean = false;
+  new_users: any[] = [];
+  semesters: any[] = [];
+
   title: string = "Todos";
+  isLoaded: boolean = false;
   breadCrumbItems: Array<{}>;
 
-  constructor(private http: HttpClient) { }
+  constructor(private semesterService: SemesterService, private userService: UserService) { }
 
   ngOnInit(): void {
     this.breadCrumbItems = [{ label: 'Usuarios' }, { label: 'Administrar Usuarios', active: true }];
-    this.getSemestersData().subscribe(data => { this.semesters = Object.values(data)[0]; });
-    this.getUsersData().subscribe(data => {
-      this.users = Object.values(data)[0];
-      this.fakeData(this.users);
-      this.number_projects = this.fake.length;
-      this.filter = this.fake;
-      this.isLoaded = true;
+    this.semesterService.getSemestersData().subscribe({ error: (err) => console.log(err), next: (rest) => this.semesters = rest.data });
+    this.userService.getUsersData().subscribe({ 
+      error: (err) => console.log(err), 
+      next: (rest) => {
+        this.users = rest.data;
+        this.new_users = this.newUserData(this.users);
+        this.number_users = this.new_users.length;
+        this.filter = this.new_users;
+        this.isLoaded = true;
+        console.log(this.new_users);
+      }
     });
   }
 
-  getUsersData() {
-    return this.http.get("http://localhost:30/api/v1.0/users/");
+  onAllFilter() {
+    this.filter = this.new_users;
+    this.number_users = this.filter.length;
   }
 
-  getSemestersData() {
-    return this.http.get("http://localhost:30/api/v1.0/semester/");
-  }
-  
-  FiltrarTodos(): void {
-    this.filter = this.fake;
-    this.number_projects = this.filter.length;
+  onSemesterFilter(semester: number) {
+    this.filter = this.new_users.filter(function(item){return (item.semester == semester);});
+    this.number_users = this.filter.length;
   }
 
-  FiltrarSemestre(semester): void {
-    this.filter = this.fake.filter(function(item){return (item.semester == semester);});
-    this.number_projects = this.filter.length;
+  onStatusFilter(status: number) {
+    this.filter = this.new_users.filter(function(item){return item.status == status;});
+    this.number_users = this.filter.length;
   }
 
-  FiltrarEstado(status): void {
-    this.filter = this.fake.filter(function(item){return item.status == status;});
-    this.number_projects = this.filter.length;
+  newUserData(users: any) {
+    var count = 0; var newusers = []
+    while(count < this.users.length) {
+      var body = { code: users[count].code, semester: Math.floor(Math.random() * (this.semesters.length - 1 + 1) + 1), 
+        status: Math.floor(Math.random() * (1 - 0 + 1) + 0) };
+      newusers.push(body);
+      count++;
+    }
+    return newusers;
   }
 
-  semesterData(semester): string {
+  onSearchFilter(keyword: string) {
+    this.filter = this.new_users.filter(function(item){ var code = item.code;
+      return (code.toLowerCase().includes(keyword.toLowerCase()));
+    });
+    this.number_users = this.filter.length;
+  }
+
+  semesterData(semester: number): string {
     var viewsemester = this.semesters.filter(function(data){ return data.id == semester })[0];
     return viewsemester.name;
   }
 
-  statusData(status): string {
-    return status == 0 ? 'Bloqueado' : 'Habilitado';
-  }
-
-  fakeData(users) {
-    var count = 0;
-    while (count < users.length) {
-      var semester = Math.floor(Math.random() * (7 - 1 + 1) + 1);
-      var status = Math.floor(Math.random() * (1 - 0 + 1) + 0);
-      var body = { code: users[count].code, semester: semester, status: status };
-      this.fake.push(body);
-      count++;
-    }
-  }
-
-  onSearchFilter(keyword: string) {
-    this.filter = this.fake.filter(function(item){
-      var code = item.code;
-      return (code.toLowerCase().includes(keyword.toLowerCase()));
-    });
-    this.number_projects = this.filter.length;
-  }
 }
