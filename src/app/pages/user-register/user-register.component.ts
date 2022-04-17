9565386addcb73493637b41e19d410e7d3654268
Albert Mayta Segovia <54085998+MSAlbert40@users.noreply.github.com>
2022-04-17 12:06:02 -0500

    fix: user-register - service connection
    Implementación de los servicios refactorizados para este proceso.

--- src/app/pages/user-register/user-register.component.ts
+++ src/app/pages/user-register/user-register.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
+import { CareerService } from 'src/app/core/services/career.service';
+import { UserService } from 'src/app/core/services/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-user-register',
@@ -11,46 +11,48 @@ import { Router } from '@angular/router';
 })
 export class UserRegisterComponent implements OnInit {
 
-  name: string = "";
-  code: string = "";
-  sede: string = "M";
-  type: string = "alumno";
+  name: string = '';
+  code: string = '';
+  sede: string = 'M';
+  score: number = 0;
   studies: number = 3;
-  teachertype: string = "PM";
-  score: number;
-  breadCrumbItems: Array<{}>;
+  type: string = 'alumno';
+  teachertype: string = 'PM';
 
-  constructor(private router: Router, private http: HttpClient) { }
+  careers: any[] = [];
+  breadCrumbItems: Array<{}>; 
+
+  constructor(private router: Router, private careerService: CareerService, private userService: UserService) { }
 
   ngOnInit(): void {
     this.breadCrumbItems = [{ label: 'Usuarios' }, { label: 'Añadir Usuario', active: true }];
+    this.careerService.getCareersData().subscribe({ error: (err) => console.log(err), next: (rest) => this.careers = rest.data });
   }
 
   successmsg() {
-    var body = {"code":"","firstname":"","lastname":"","role_id":1,"career_id": 1};
-    body["code"] = this.code;
+    var body = { code: '', firstname: '', lastname: '', role_id: 0, career_id: 0 };
+    body['code'] = this.code;
     for (var i = 0; i < this.name.split(" ").length-2; i++) {
-      body["firstname"] += this.name.split(" ")[i];
-      body["firstname"] += " ";
+      body['firstname'] += this.name.split(" ")[i];
+      body['firstname'] += " ";
     }
-    body["lastname"] = this.name.split(" ")[this.name.split(" ").length-2] + " " + this.name.split(" ")[this.name.split(" ").length-1];
-    if (this.type == "alumno") { body["role_id"] = 1; } 
-    else if (this.type == "docente") { body["role_id"] = 4; } 
-    else if (this.type == "mcomite") { body["role_id"] = 5; }
-    body["career_id"] = this.studies;
-    this.postUser(body).subscribe(result => {
-      console.log(result);
-    });;
-    Swal.fire({
-      title: 'Usuario Registrado',
-      text: 'Se ha enviado las instrucciones al usuario generado',
-      icon: 'success',
-      confirmButtonColor: '#EF360E',
-    });
-  }
-
-  postUser(body) {
-    return this.http.post('localhost:3000/api/v1.0/users/save', body);
+    body['lastname'] = this.name.split(" ")[this.name.split(" ").length-2] + " " + this.name.split(" ")[this.name.split(" ").length-1];
+    if (this.type == "alumno") { body['role_id'] = 1; } 
+    else if (this.type == "docente") { body['role_id'] = 4; } 
+    else if (this.type == "mcomite") { body['role_id'] = 5; }
+    body['career_id'] = this.studies;
+    /*
+    this.userService.saveUser(body).subscribe({
+      error: (err) => console.log(err), 
+      next: (rest) => {
+        Swal.fire({
+          title: 'Usuario Registrado',
+          text: 'Se ha enviado las instrucciones al usuario generado',
+          icon: 'success',
+          confirmButtonColor: '#EF360E',
+        });
+      }
+    })*/
   }
 
   buckupload() {
