eb9bc29a408417ac8e1a69e0fd98efd3d5f0ef43
Albert Mayta Segovia <54085998+MSAlbert40@users.noreply.github.com>
2022-04-17 09:50:06 -0500

    fix: group-view - service connection
    Implementación de los servicios refactorizados para este proceso.

--- src/app/pages/group-view/group-view.component.ts
+++ src/app/pages/group-view/group-view.component.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { GroupService } from 'src/app/core/services/group.service';
+import { ProjectService } from 'src/app/core/services/project.service';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-group-view',
@@ -10,32 +13,65 @@ import { Router } from '@angular/router';
 export class GroupViewComponent implements OnInit {
 
   user: any;
+  group: any;
   project: any;
+  partner: any;
+
+  users: any[] = [];
+  groups: any[] = [];
+  projects: any[] = [];
+
   assigned: boolean = true;
   isLoaded: boolean = true;
   breadCrumbItems: Array<{}>;
 
-  constructor(private router: Router, private http: HttpClient) { }
+  constructor(private router: Router, private userService: UserService, 
+    private projectService: ProjectService, private groupService: GroupService) { }
 
   ngOnInit(): void {
     this.breadCrumbItems = [{ label: 'Grupos' }, { label: 'Visualizar Mi Grupo', active: true }];
-    var currentUser = JSON.parse(localStorage.getItem("currentUser")!);
-    this.user = currentUser.data;
-    this.getProjectsData().subscribe(data => {
-      var projects = Object.values(data)[0];
-      this.project = projects[0];
+    this.user = JSON.parse(localStorage.getItem('currentUser')!).user.information;
+    this.searchPartnerData(this.group);
+    this.searchProjectData(this.group);
+    /*this.groupService.getGroupsData().subscribe({  
+      error: (err) => console.log(err), 
+      next: (rest) => { 
+        this.groups = rest.data;
+        this.group = this.group.filter(function(data){ return data.student_1_id == this.user.id || data.student_2_id == this.user.id })[0];
+        this.searchPartnerData(this.group);
+        if (this.group) this.isLoaded = true; 
+      }
+    });*/
+  }
+
+  searchPartnerData(group: any) {
+    //var partner_id = (this.user.id == group.student_1_id ? group.student_1_id : group.student_2_id);
+    this.userService.getUsersData().subscribe({
+      error: (err) => console.log(err), 
+      next: (rest) => { 
+        this.users = rest.data;
+        this.partner = this.users.filter(function(data){ return data.id == '3' })[0];
+        //console.log(this.partner);
+      }
     });
   }
 
-  getProjectsData() {
-    return this.http.get("http://localhost:30/api/v1.0/projects");
+  searchProjectData(group: any) {
+    //var project_id = group.project_assigned;
+    this.projectService.getProjectsData().subscribe({
+      next: (rest) => { 
+        this.projects = rest.data;
+        this.project = this.projects.filter(function(data){ return data.id == '3' })[0];
+        //console.log(this.project);
+      }
+    });
   }
 
   groupregister() {
     this.router.navigate(['/group-register']);
   }
 
-  gotodetails(code) {
+  gotodetails(code: string) {
     this.router.navigate(['/project-details-portfolio/'+code]);
   }
 
