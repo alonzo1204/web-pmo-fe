08274829d63b33d9b219ca3b809187543d57d511
Albert Mayta Segovia <54085998+MSAlbert40@users.noreply.github.com>
2022-04-17 12:08:02 -0500

    fix: user-details - service connection
    Implementación de los servicios refactorizados para este proceso.

--- src/app/pages/user-details/user-details.component.ts
+++ src/app/pages/user-details/user-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-user-details',
@@ -13,20 +13,19 @@ export class UserDetailsComponent implements OnInit {
   isLoaded: Boolean = false;
   breadCrumbItems: Array<{}>;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+  constructor(private route: ActivatedRoute, private userService: UserService) { }
 
   ngOnInit(): void {
     this.breadCrumbItems = [{ label: 'Usuarios' }, { label: 'Lista de Usuarios' }, { label: 'Detalles', active: true }];
     var code = this.route.snapshot.params.code;
-    this.getUsersData().subscribe(data => {
-      this.users = Object.values(data)[0];
-      this.user = this.users.filter(function(data){ return data.code == code })[0];
-      this.isLoaded = true;
+    this.userService.getUsersData().subscribe({
+      error: (err) => console.log(err), 
+      next: (rest) => { 
+        this.users = rest.data;
+        this.user = this.users.filter(function(data){ return data.code == code })[0];
+        this.isLoaded = true;
+      }
     });
-  }
-
-  getUsersData() {
-    return this.http.get("http://localhost:30/api/v1.0/users/");
   }
 
 }
