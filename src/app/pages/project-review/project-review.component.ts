e916b4e269c75eb48b4dfe93ca258ff5b287e469
Albert Mayta Segovia <54085998+MSAlbert40@users.noreply.github.com>
2022-04-17 07:59:36 -0500

    fix: project-review - service connection
    Implementación de los servicios refactorizados para este proceso.

--- src/app/pages/project-review/project-review.component.ts
+++ src/app/pages/project-review/project-review.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input} from '@angular/core';
-import {Router} from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { ProjectService } from 'src/app/core/services/project.service';
 
 @Component({
   selector: 'app-project-review',
@@ -8,46 +8,39 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./project-review.component.scss']
 })
 export class ProjectReviewComponent implements OnInit {
+
   page = 1;
-  number_projects: number = 50;
   pageSize = 10;
-  keyword: string = "";
   projects: any[] = [];
   filter: any[] = [];
+  keyword: string = '';
+  number_projects!: number;
   isLoaded: boolean = false;
   breadCrumbItems: Array<{}>;
 
-  constructor(private router: Router, private http: HttpClient) { }
+  constructor(private router: Router, private projectService: ProjectService) { }
 
   ngOnInit(): void {
-    this.filter = this.projects;
-    this.getProjectsData().subscribe(data => {
-      this.projects = Object.values(data)[0];
-      this.number_projects = this.projects.length;
-      this.filter = this.projects;
-      this.isLoaded = true;
-    });
     this.breadCrumbItems = [{ label: 'Proyectos' }, { label: 'Revisión de Proyectos', active: true }];
+    this.projectService.getProjectsData().subscribe({ 
+      error: (err) => console.log(err), 
+      next: (rest) => { 
+        this.projects = rest.data;
+        this.number_projects = this.projects.length;
+        this.filter = this.projects;
+        this.isLoaded = true;
+      } 
+    });
   }
 
-  getProjectsData() {
-    return this.http.get("http://localhost:30/api/v1.0/projects");
-  }
-
-  FiltrarTodos(): void {
-    this.filter = this.projects;
-  }
-
-  FiltrarAprobados(): void {
-    this.filter = this.projects.filter(function(item){return item.project_process_state.id == 2;});
-  }
-
-  FiltrarPendientes(): void {
-    this.filter = this.projects.filter(function(item){return item.project_process_state.id == 1;});
-  }
-
-  FiltrarRechazados(): void {
-    this.filter = this.projects.filter(function(item){return item.project_process_state.id == 3;});
+  onStatusFilter(id: number) {
+    switch(id) {
+      case 1: this.filter = this.projects.filter(function(item){ return item.project_process_state.id == 1; }); break;
+      case 2: this.filter = this.projects.filter(function(item){ return item.project_process_state.id == 2; }); break;
+      case 3: this.filter = this.projects.filter(function(item){ return item.project_process_state.id == 3; }); break;
+      default: this.filter = this.projects;
+    }
+    this.number_projects = this.filter.length;
   }
 
   onSearchFilter(keyword: string) {
@@ -57,9 +50,10 @@ export class ProjectReviewComponent implements OnInit {
       item.company.name.toLowerCase().includes(keyword.toLowerCase()) || 
       item.career.name.toLowerCase().includes(keyword.toLowerCase()));
     });
+    this.number_projects = this.filter.length;
   }
 
-  gotodetails(id) {
+  gotodetails(id: number) {
     this.router.navigate(['/project-details-review/'+this.projects[id-1].code]);
   }
 
--- src/app/pages/projects-list/projects-list.component.ts
+++ src/app/pages/projects-list/projects-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { ProjectService } from 'src/app/core/services/project.service';
 
 @Component({
