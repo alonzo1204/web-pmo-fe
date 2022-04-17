3482317485ab592e3d17a2ff108ecf843067e3c7
Albert Mayta Segovia <54085998+MSAlbert40@users.noreply.github.com>
2022-04-17 00:33:32 -0500

    fix: profile - add connection with localstorage
    Implementación de nuevos datos recepcionados del endpoint login mediante el localstorage.

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -9,26 +9,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
   user: any;
-  users: any[] = [];
   isLoaded: Boolean = false;
   breadCrumbItems: Array<{}>;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+  constructor(private route: ActivatedRoute, private userService: UserService) { }
 
   ngOnInit(): void {
-    var currentUser = JSON.parse(localStorage.getItem("currentUser")!);
     this.breadCrumbItems = [{ label: 'Perfil' }, { label: 'Datos Personales', active: true }];
-    //var code = this.route.snapshot.params.code;
-    var code = currentUser.data.code;
-    this.getUsersData().subscribe(data => {
-      this.users = Object.values(data)[0];
-      this.user = this.users.filter(function(data){ return data.code == code })[0];
-      this.isLoaded = true;
-    });
-  }
-
-  getUsersData() {
-    return this.http.get("http://localhost:30/api/v1.0/users/");
+    this.user = JSON.parse(localStorage.getItem("currentUser")!).user.information;
+    if (this.user != undefined) this.isLoaded = true;
   }
 
 }
