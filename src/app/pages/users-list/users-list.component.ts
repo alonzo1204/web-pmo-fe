a92ede8acf8e4c715c1d9a4503ca8e744a948c25
Albert Mayta Segovia <54085998+MSAlbert40@users.noreply.github.com>
2022-04-17 11:53:01 -0500

    fix: user-list - service connection
    Implementación de los servicios refactorizados para este proceso.

--- src/app/pages/users-list/users-list.component.ts
+++ src/app/pages/users-list/users-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-users-list',
@@ -8,80 +8,69 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./users-list.component.scss']
 })
 export class UsersListComponent implements OnInit {
+ 
   page = 1;
-  number_projects: number = 50;
   pageSize = 15;
+  number_users: number;
   keyword: string = "";
   users: any[] = [];
   filter: any[] = [];
-  isLoaded: boolean = false;
+  level: number;
   title: string = "Todos";
+  isLoaded: boolean = false;
   breadCrumbItems: Array<{}>;
 
-  level: number;
-
-  constructor(private router: Router, private http: HttpClient) { }
+  constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit(): void {
     this.breadCrumbItems = [{ label: 'Usuarios' }, { label: 'Lista de Usuarios', active: true }];
-    this.getUsersData().subscribe(data => {
-      this.users = Object.values(data)[0];
-      this.number_projects = this.users.length;
-      this.filter = this.users;
-      this.isLoaded = true;
+    this.userService.getUsersData().subscribe({
+      error: (err) => console.log(err), 
+      next: (rest) => { 
+        this.users = rest.data;
+        this.filter = this.users;
+        this.number_users = this.users.length;
+        this.isLoaded = true;
+      }
     });
   }
 
-  getUsersData() {
-    return this.http.get("http://localhost:30/api/v1.0/users/");
-  }
-
-  FiltrarTodos(): void {
-    this.filter = this.users;
-  }
-
-  FiltrarAlumnos(): void {
-    this.filter = this.users.filter(function(item){return (item.role.name == "tdp" || item.role.name == "tp1" || item.role.name == "tp2");});
-  }
-
-  FiltrarDocentes(): void {
-    this.filter = this.users.filter(function(item){return item.role.name == "docente";});
-  }
-
-  FiltrarComites(): void {
-    this.filter = this.users.filter(function(item){return item.role.name == "comite";});
-  }
-
-  FiltrarInactivos(): void {
-    this.filter = this.users.filter(function(item){return item.active == 0;});
+  onRoleFilter(id: number) {
+    switch(id) {
+      case 1: this.filter = this.users.filter(function(item){ return (item.role.name == "tdp" || item.role.name == "tp1" || item.role.name == "tp2"); }); break;
+      case 2: this.filter = this.users.filter(function(item){ return item.role.name == "docente" }); break;
+      case 3: this.filter = this.users.filter(function(item){ return item.role.name == "comite"; }); break;
+      case 4: this.filter = this.users.filter(function(item){ return item.active == 0; }); break;
+      default: this.filter = this.users;
+    }
+    this.number_users = this.filter.length;
   }
 
   onSearchFilter(keyword: string) {
     this.filter = this.users.filter(function(item){
       var username = item.firstname + " " + item.lastname;
       return (username.toLowerCase().includes(keyword.toLowerCase()) || 
-      item.code.toLowerCase().includes(keyword.toLowerCase()) || 
-      item.role.name.toLowerCase().includes(keyword.toLowerCase()));
+      item.code.toLowerCase().includes(keyword.toLowerCase()));
     });
+    this.number_users = this.filter.length; 
   }
 
-  gotodetails(id) {
-    this.router.navigate(['/user-details/'+this.users[id-1].code]);
-  }
-
-  averageStudent(min, max): string {
+  averageStudent(min: number, max: number): string {
     var average = Math.floor(Math.random() * (max - min + 1) + min);
     return average.toFixed(2);
   }
 
-  englishLevel(min, max): number {
+  englishLevel(min: number, max: number): number {
     this.level = Math.floor(Math.random() * (max - min + 1) + min);
     return this.level;
   }
 
-  englishLabel(level): string {
-    if (level == 5) return level + ' - apto'
-    else return level + ' - no apto'
+  englishLabel(level: number): string {
+    return level == 5 ? level + '- no apto' : level + ' - apto'
+  }
+
+  gotodetails(id: number) {
+    this.router.navigate(['/user-details/'+this.users[id-1].code]);
   }
 
 }
