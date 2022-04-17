9310e25397ff1d21ac810c0352981f2aed252132
Albert Mayta Segovia <54085998+MSAlbert40@users.noreply.github.com>
2022-04-17 02:20:51 -0500

    fix: projects-list - service connection
    Implementación de los servicios refactorizados para este proceso.

--- src/app/pages/projects-list/projects-list.component.ts
+++ src/app/pages/projects-list/projects-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { ProjectService } from 'src/app/core/services/project.service';
 
 @Component({
   selector: 'app-projects-list',
@@ -8,43 +9,38 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./projects-list.component.scss']
 })
 export class ProjectsListComponent implements OnInit {
+  
   page = 1;
-  number_projects: number = 50;
   pageSize = 10;
-  keyword: string = "";
   projects: any[] = [];
   filter: any[] = [];
+  keyword: string = '';
+  number_projects!: number;
   isLoaded: boolean = false;
-  title: string = "Todos";
   breadCrumbItems: Array<{}>;
 
-  constructor(private router: Router, private http: HttpClient) { }
+  constructor(private router: Router, private projectService: ProjectService) { }
 
   ngOnInit(): void {
-    this.filter = this.projects;
-    this.getProjectsData().subscribe(data => {
-      this.projects = Object.values(data)[0];
-      this.number_projects = this.projects.length;
-      this.filter = this.projects;
-      this.isLoaded = true;
+    this.breadCrumbItems = [{ label: 'Asignación de Docentes' }, { label: 'Lista de Proyectos', active: true }]; 
+    this.projectService.getProjectsData().subscribe({ 
+      error: (err) => console.log(err), 
+      next: (rest) => { 
+        this.projects = rest.data;
+        this.number_projects = this.projects.length;
+        this.filter = this.projects;
+        this.isLoaded = true;
+      } 
     });
-    this.breadCrumbItems = [{ label: 'Asignación de Docentes' }, { label: 'Lista de Proyectos', active: true }];
   }
 
-  getProjectsData() {
-    return this.http.get("http://localhost:30/api/v1.0/projects");
-  }
-
-  FiltrarTodos(): void {
-    this.filter = this.projects;
-  }
-
-  FiltrarAsignados(): void {
-    this.filter = this.projects.filter(function(item){return item.project_process_state.id == 2;});
-  }
-
-  FiltrarPendientes(): void {
-    this.filter = this.projects.filter(function(item){return item.project_process_state.id == 1;});
+  onStatusFilter(id: number) {
+    switch(id) {
+      case 1: this.filter = this.projects.filter(function(item){ return item.project_process_state.id == 1; }); break;
+      case 2: this.filter = this.projects.filter(function(item){ return item.project_process_state.id == 2; }); break;
+      default: this.filter = this.projects;
+    }
+    this.number_projects = this.filter.length;
   }
 
   onSearchFilter(keyword: string) {
@@ -54,9 +50,10 @@ export class ProjectsListComponent implements OnInit {
       item.company.name.toLowerCase().includes(keyword.toLowerCase()) || 
       item.career.name.toLowerCase().includes(keyword.toLowerCase()));
     });
+    this.number_projects = this.filter.length;
   }
 
-  gotodetails(id) {
+  gotodetails(id: number) {
     this.router.navigate(['/project-details-asignation/'+this.projects[id-1].code]);
   }
 
