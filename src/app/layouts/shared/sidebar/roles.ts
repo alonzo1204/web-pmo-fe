0a0d546cf87c05c9d32767dff46708693fede937
Albert Mayta Segovia <54085998+MSAlbert40@users.noreply.github.com>
2022-04-17 19:28:45 -0500

    feat: add roles constant
    Implementación de archivo que contiene los accesos acorde al rol del usuario.

---
+++ src/app/layouts/shared/sidebar/roles.ts
@@ -0,0 +1,27 @@
+export const ROLES: any[] = [
+    { 
+        id: 1, 
+        name: 'tdp', 
+        access: [1] 
+    },
+    { 
+        id: 2, 
+        name: 'tp1', 
+        access: [1, 4, 5] 
+    },
+    { 
+        id: 3, 
+        name: 'tp2', 
+        access: [1, 4, 5] 
+    },
+    { 
+        id: 4, 
+        name: 'docente', 
+        access: [1, 2, 3] 
+    },
+    { 
+        id: 5, 
+        name: 'comite', 
+        access: [1, 2, 3, 6] 
+    }
+];
