f509e11ae9928bbeef2fccc9a095fa4b088e0b58
Albert Mayta Segovia <54085998+MSAlbert40@users.noreply.github.com>
2022-04-23 10:34:18 -0500

    fix: projects-list - modification of the data process
    Implementación de nuevas funciones identificadoras de datos por id, adecuada implementación del servicio de filtrado.

--- src/app/pages/projects-list/projects-list.component.ts
+++ src/app/pages/projects-list/projects-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CareerService } from 'src/app/core/services/career.service';
+import { CompanyService } from 'src/app/core/services/company.service';
 import { ProjectService } from 'src/app/core/services/project.service';
 
 @Component({
@@ -18,11 +20,17 @@ export class ProjectsListComponent implements OnInit {
   isLoaded: boolean = false;
   breadCrumbItems: Array<{}>;
 
-  constructor(private router: Router, private projectService: ProjectService) { }
+  careers: any[] = [];
+  companies: any[] = [];
+
+  constructor(private router: Router, private projectService: ProjectService,
+              private companyService: CompanyService, private careerService: CareerService) { }
 
   ngOnInit(): void {
     this.breadCrumbItems = [{ label: 'Asignación de Docentes' }, { label: 'Lista de Proyectos', active: true }]; 
-    this.projectService.getProjectsbyStatus([5, 6]).subscribe({ 
+    this.companyService.getCompaniesData().subscribe({ error: (err) => console.log(err), next: (rest) => this.companies = rest.data });
+    this.careerService.getCareersData().subscribe({ error: (err) => console.log(err), next: (rest) => this.careers = rest.data });
+    this.projectService.getProjectsbyStatusVarius([5, 6]).subscribe({ 
       error: (err) => console.log(err), 
       next: (rest) => { 
         this.projects = rest.data;
@@ -35,11 +43,32 @@ export class ProjectsListComponent implements OnInit {
 
   onStatusFilter(id: number) {
     switch(id) {
-      case 1: this.filter = this.projects.filter(function(item){ return item.project_process_state.id == 1; }); break;
-      case 2: this.filter = this.projects.filter(function(item){ return item.project_process_state.id == 2; }); break;
+      case 1: this.filter = this.projects.filter(function(item){ return item.project_process_state_id == 5; }); break;
+      case 2: this.filter = this.projects.filter(function(item){ return item.project_process_state_id == 6; }); break;
       default: this.filter = this.projects;
     }
     this.number_projects = this.filter.length;
+  }
+
+  nameStatus(id: number): string {
+    switch(id) {
+      case 1: return 'Pendiente de aprobación'; break;
+      case 2: return 'Aprobado'; break;
+      case 3: return 'Denegado'; break;
+      case 4: return 'Aceptado con comentarios'; break;
+      case 5: return 'Asignado'; break;
+      default: return 'Pendiente de asignación de docentes';
+    }
+  }
+
+  nameCareer(id: number): string {
+    var career = this.careers.filter(function(item){ return item.id = id })[0];
+    return career.name;
+  }
+
+  imageCompany(id: number): string {
+    var company = this.companies.filter(function(item){ return item.id = id })[0];
+    return company.image;
   }
 
   onSearchFilter(keyword: string) {
