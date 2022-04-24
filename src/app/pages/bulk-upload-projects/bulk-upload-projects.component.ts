6f913846de6e1a924f9ac061d63859cfbbdcaf80
Albert Mayta Segovia <54085998+MSAlbert40@users.noreply.github.com>
2022-04-24 14:47:09 -0500

    fix: bulk-upload-projects - service integration
    Implementación en proceso en carga masiva de proyectos.

--- src/app/pages/bulk-upload-projects/bulk-upload-projects.component.ts
+++ src/app/pages/bulk-upload-projects/bulk-upload-projects.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { DropzoneConfigInterface } from 'ngx-dropzone-wrapper';
+import { ProjectService } from 'src/app/core/services/project.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-bulk-upload-projects',
@@ -8,12 +12,57 @@ import { Router } from '@angular/router';
 })
 export class BulkUploadProjectsComponent implements OnInit {
 
+  form: FormGroup;
+  button_state: boolean = false;
   breadCrumbItems: Array<{}>;
 
-  constructor(private router: Router) { }
+  config: DropzoneConfigInterface = {
+    clickable: true,
+    maxFiles: 1,
+  }
+
+  constructor(private router: Router, private formBuilder: FormBuilder, private projectService: ProjectService) { 
+    this.form = this.formBuilder.group({
+      file: [null]
+    });
+  }
 
   ngOnInit(): void {
     this.breadCrumbItems = [{ label: 'Proyectos' }, { label: 'Añadir Proyecto'}, { label: 'Carga Masiva de Proyectos', active: true }];
+  }
+
+  onUploadSuccess(event) {
+    const file = event[0];
+    this.form.patchValue({ file: file });
+    this.form.get('file').updateValueAndValidity();
+  }
+
+  successmsg() {
+    var formData: any = new FormData();
+    formData.append('file', this.form.get('file').value);
+    //console.log(this.form.get('file').value)
+    this.button_state = true;
+    this.projectService.saveMasiveRegister(formData).subscribe({
+      error: (err) => {
+        console.log(err),
+        this.button_state = false;
+        Swal.fire({
+          title: 'Archivo Excel no pudo subido',
+          text: 'Verifique llenar los campos correctamente',
+          icon: 'error',
+          confirmButtonColor: '#E42322',
+        });
+      },
+      next: (rest) => {
+        Swal.fire({
+          title: 'Archivo Excel subido',
+          text: 'El archivo Excel ha sido subido exitosamente',
+          icon: 'success',
+          confirmButtonColor: '#EF360E',
+        });
+      }, 
+      complete: () => this.button_state = false
+    });
   }
 
   goback() {
