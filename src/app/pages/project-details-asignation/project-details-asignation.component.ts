91c6cf57b5af96b92b1eaf1f84e8646d36c87723
Albert Mayta Segovia <54085998+MSAlbert40@users.noreply.github.com>
2022-05-01 14:10:57 -0500

    fix: loading view - delete template
    Corrección de loading que utiliza un template no funcional.

--- src/app/pages/project-details-asignation/project-details-asignation.component.ts
+++ src/app/pages/project-details-asignation/project-details-asignation.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { timeStamp } from 'console';
 import { ProjectService } from 'src/app/core/services/project.service';
 import Swal from 'sweetalert2';
 
