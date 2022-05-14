32757491377daaa02f07f2b5923f00e9272eb603
time2w8 <emgore.1410@gmail.com>
2022-05-14 13:50:18 -0500

    feat: new permissions

--- src/app/layouts/shared/sidebar/roles.ts
+++ src/app/layouts/shared/sidebar/roles.ts
@@ -1,27 +1,27 @@
 export const ROLES: any[] = [
-    { 
-        id: 1, 
-        name: 'tdp', 
-        access: [1] 
+    {
+        id: 1,
+        name: 'tdp',
+        access: [1]
     },
-    { 
-        id: 2, 
-        name: 'tp1', 
-        access: [1, 3, 6] 
+    {
+        id: 2,
+        name: 'tp1',
+        access: [1, 3, 6]
     },
-    { 
-        id: 3, 
-        name: 'tp2', 
-        access: [1, 3, 6] 
+    {
+        id: 3,
+        name: 'tp2',
+        access: [1, 3, 6]
     },
-    { 
-        id: 4, 
-        name: 'docente', 
-        access: [1, 2, 7] 
+    {
+        id: 4,
+        name: 'docente',
+        access: [1, 2, 7]
     },
-    { 
-        id: 5, 
-        name: 'comite', 
-        access: [1, 2, 4, 5, 8, 9, 10, 11, 12] 
+    {
+        id: 5,
+        name: 'comite',
+        access: [1, 2, 4, 5, 7, 8, 9, 10, 11, 12]
     }
 ];
