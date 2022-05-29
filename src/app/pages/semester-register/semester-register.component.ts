6c49d9c02fbd3ce19ebc4666115aff89e6f2f966
Albert Mayta Segovia <54085998+MSAlbert40@users.noreply.github.com>
2022-05-29 10:31:19 -0500

    feat: Integration service semester-register
    Integración del servicio para agregar semestres, incluyendo validaciones de campos, bloqueo temporales de botones y redirección de post-proceso

--- src/app/pages/semester-register/semester-register.component.ts
+++ src/app/pages/semester-register/semester-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { SemesterService } from 'src/app/core/services/semester.service';
 import Swal from 'sweetalert2';
 
@@ -10,45 +11,70 @@ import Swal from 'sweetalert2';
 export class SemesterRegisterComponent implements OnInit {
 
   name: string = '';
+  date_from: string = '';
+  date_until: string = '';
   button_state: boolean = false;
   loading: boolean = false;
   breadCrumbItems: Array<{}>; 
 
-  constructor(private semesterService: SemesterService) { }
+  constructor(private semesterService: SemesterService, private router: Router) { }
 
   ngOnInit(): void {
     this.breadCrumbItems = [{ label: 'Semestres' }, { label: 'Añadir Semestre', active: true }]; 
   }
 
   validate(): boolean {
-    if (this.name == '') {
-      return false;
-    }
+    if (this.name == '' || this.date_from == '' || this.date_until == '') { return false; }
     return true
   }
 
   successmsg() {
-    var body = { name: '' };
-    body['name'] = this.name;
-    /*
-    this.button_state = true;
-    this.loading = true;
-    this.semesterService.saveSemester(body).subscribe({
-      error: (err) => { 
-        this.loading = false;
-        this.button_state = false;
-      }, 
-      next: (rest) => {
-        Swal.fire({
-          title: 'Semestre Registrado',
-          text: 'El semestre ha sido registrado exitosamente',
-          icon: 'success',
-          confirmButtonColor: '#EF360E',
-        });
-        this.loading = false;
-        this.button_sate = false;
-      }
-    });*/
+    if(this.validate()) {
+      var body = { name: '', date_from: '', date_until: '' };
+      body['name'] = this.name;
+      body['date_from'] = this.date_from;
+      body['date_until'] = this.date_until;
+      this.button_state = true;
+      this.loading = true;
+      this.semesterService.saveSemester(body).subscribe({
+        error: (err) => { 
+          this.loading = false;
+          this.button_state = false;
+          Swal.fire({
+            title: 'Semestre no pudo Registrarse',
+            text: err,
+            icon: 'error',
+            confirmButtonColor: '#E42322',
+          });
+        }, 
+        next: (rest) => {
+          Swal.fire({
+            title: 'Semestre Registrado',
+            text: 'El semestre ha sido registrado exitosamente',
+            icon: 'success',
+            confirmButtonColor: '#EF360E',
+            onClose: () => {
+              this.router.navigate(['/semesters-list']);
+            }
+          });
+          this.loading = false;
+          this.button_state = false;
+        }
+      });
+    } else {
+      Swal.fire({
+        title: 'Complete el formulario',
+        text: 'Todos los campos son obligatorios',
+        icon: 'error',
+        confirmButtonColor: '#E42322',
+      });
+    }
+  }
+
+  cleanprocess() {
+    this.loading = false;
+    this.button_state = false;
+    this.name = ''; this.date_from = ''; this.date_until = '';
   }
 
 }
