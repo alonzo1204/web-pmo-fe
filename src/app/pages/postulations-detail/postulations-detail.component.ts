4a963ef689dc50ffaf33e4a6a7bec9ed5ee2e01b
Albert Mayta Segovia <54085998+MSAlbert40@users.noreply.github.com>
2022-06-12 10:24:24 -0500

    fix: Fixed Postulations History in detail of postulation
    Correción de historial de postulaciones.

--- src/app/pages/postulations-detail/postulations-detail.component.ts
+++ src/app/pages/postulations-detail/postulations-detail.component.ts
@@ -10,10 +10,9 @@ import { ProjectService } from 'src/app/core/services/project.service';
 })
 export class PostulationsDetailComponent implements OnInit {
 
-  records: any;
+  newcode: any;
   mypostulations: any;
   projects: any[] = [];
-  pprojects: any[] = [];
   postulations: any[] = [];
   loading: boolean = false;
   isLoaded: boolean = false;
@@ -25,13 +24,13 @@ export class PostulationsDetailComponent implements OnInit {
   ngOnInit(): void {
     this.breadCrumbItems = [{ label: 'Postulaciones' }, { label: 'Lista de Postulaciones'}, { label: 'Detalle', active: true }];
     var code = this.route.snapshot.params.code;
+    this.newcode = code;
     this.loading = true;
     this.postulationService.getPostulationsData().subscribe({
       error: (err) => this.loading = false,
       next: (rest) => {
         this.postulations = rest.data;
         this.mypostulations = this.postulations.filter(function (data) { return data.id == code })[0];
-        this.historyPostulations(this.mypostulations.id);
         this.searchProjectData(this.mypostulations.p1.id);
         this.searchProjectData(this.mypostulations.p2.id);
         this.searchProjectData(this.mypostulations.p4.id);
@@ -39,33 +38,6 @@ export class PostulationsDetailComponent implements OnInit {
         this.loading = false;
       }
     })
-  }
-
-  historyPostulations(id) {
-    let params = { id_postulation_row: id }
-    this.projectService.getHistoryProjects(params).subscribe({
-      error: (err) => this.loading = false,
-      next: (rest) => {
-        this.records = rest.data;
-        this.searchProjectPData(this.records.project1.id);
-        this.searchProjectPData(this.records.project2.id);
-        this.searchProjectPData(this.records.project3.id);
-        this.searchProjectPData(this.records.project4.id);
-        this.isLoaded = true;
-        this.loading = false;
-        console.log(rest)
-      }
-    });
-  }
-
-  searchProjectPData(project: any) {
-    //var project_id = group.project_assigned;
-    this.projectService.getProjectsData().subscribe({
-      next: (rest) => { 
-        let projects = rest.data;
-        this.pprojects.push(projects.filter(function(data){ return data.id == project })[0]);
-      }
-    });
   }
 
   searchProjectData(project: any) {
@@ -76,6 +48,10 @@ export class PostulationsDetailComponent implements OnInit {
         this.projects.push(projects.filter(function(data){ return data.id == project })[0]);
       }
     });
+  }
+
+  gotoHistory() {
+    this.router.navigate(['/postulations-history/' + this.newcode]);
   }
 
   gotodetails(code: string) {
--- src/app/pages/postulations-history/postulations-history.component.ts
+++ src/app/pages/postulations-history/postulations-history.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PostulationService } from 'src/app/core/services/postulation.service';
+import { ProjectService } from 'src/app/core/services/project.service';
 
 @Component({
   selector: 'app-postulations-history',
@@ -7,9 +10,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostulationsHistoryComponent implements OnInit {
 
-  constructor() { }
+  records: any;
+  mypostulations: any;
+  projects: any[] = [];
+  pprojects: any[] = [];
+  postulations: any[] = [];
+  loading: boolean = false;
+  isLoaded: boolean = false;
+  breadCrumbItems: Array<{}>;
+
+  constructor(private route: ActivatedRoute, private router: Router, private postulationService: PostulationService, 
+    private projectService: ProjectService) { }
 
   ngOnInit(): void {
+    this.breadCrumbItems = [{ label: 'Lista de Postulaciones'}, { label: 'Historial', active: true }];
+    var code = this.route.snapshot.params.code;
+    this.loading = true;
+    this.postulationService.getPostulationsData().subscribe({
+      error: (err) => this.loading = false,
+      next: (rest) => {
+        this.postulations = rest.data;
+        this.mypostulations = this.postulations.filter(function (data) { return data.id == code })[0];
+        this.historyPostulations(this.mypostulations.id);
+        this.searchProjectData(this.mypostulations.p1.id);
+        this.searchProjectData(this.mypostulations.p2.id);
+        this.searchProjectData(this.mypostulations.p4.id);
+        this.searchProjectData(this.mypostulations.pa.id);
+        this.loading = false;
+      }
+    })
+  }
+
+  historyPostulations(id) {
+    let params = { id_postulation_row: id }
+    this.projectService.getHistoryProjects(params).subscribe({
+      error: (err) => this.loading = false,
+      next: (rest) => {
+        this.records = rest.data;
+        this.searchProjectPData(this.records.project1.id);
+        this.searchProjectPData(this.records.project2.id);
+        this.searchProjectPData(this.records.project3.id);
+        this.searchProjectPData(this.records.project4.id);
+        this.isLoaded = true;
+        this.loading = false;
+        console.log(rest)
+      }
+    });
+  }
+
+  searchProjectPData(project: any) {
+    //var project_id = group.project_assigned;
+    this.projectService.getProjectsData().subscribe({
+      next: (rest) => { 
+        let projects = rest.data;
+        this.pprojects.push(projects.filter(function(data){ return data.id == project })[0]);
+      }
+    });
+  }
+
+  searchProjectData(project: any) {
+    //var project_id = group.project_assigned;
+    this.projectService.getProjectsData().subscribe({
+      next: (rest) => { 
+        let projects = rest.data;
+        this.projects.push(projects.filter(function(data){ return data.id == project })[0]);
+      }
+    });
+  }
+
+  gotodetails(code: string) {
+    if(code != null) this.router.navigate(['/project-details-portfolio/' + code]);
+    else return;
   }
 
 }
