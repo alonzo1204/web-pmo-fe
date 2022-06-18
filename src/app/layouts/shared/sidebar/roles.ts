166891e4f75a2b7bb7ba1210d28a91153b2c0bba
Albert Mayta Segovia <54085998+MSAlbert40@users.noreply.github.com>
2022-06-18 11:08:59 -0500

    feat: Add module postulations for role 'Comite'
    Añadir el módulo de postulaciones al comite.

--- src/app/layouts/shared/sidebar/roles.ts
+++ src/app/layouts/shared/sidebar/roles.ts
@@ -22,6 +22,6 @@ export const ROLES: any[] = [
     {
         id: 5,
         name: 'comite',
-        access: [1, 2, 4, 5, 7, 8, 9, 10, 11, 12]
+        access: [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12]
     }
 ];
